61f48877b0bb1115ce5dece6e5f4fbfc4ea0e28d
Janne Mattila <janne.mattila@hotmail.fi>
2025-02-07 13:30:21 +0200

    Update installation scripts and CI

--- install-windows.ps1
+++ install-windows.ps1
@@ -7,11 +7,30 @@ Set-Location \temp\
 # The .NET Core Hosting Bundle
 # https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/iis/hosting-bundle?view=aspnetcore-9.0
 # https://dotnet.microsoft.com/permalink/dotnetcore-current-windows-runtime-bundle-installer
-Invoke-WebRequest "https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.1/dotnet-hosting-9.0.1-win.exe" -OutFile dotnet-hosting.exe
+Invoke-WebRequest "https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.1/dotnet-hosting-9.0.1-win.exe" -OutFile dotnet-hosting.exe -ProgressAction SilentlyContinue
 .\dotnet-hosting.exe /quiet
 
-Invoke-WebRequest "https://github.com/JanneMattila/webapp-network-tester/releases/download/v1.0.0/webappnetworktester-windows.zip" -OutFile webapp-network-tester.zip
+Invoke-WebRequest "https://github.com/JanneMattila/webapp-network-tester/releases/download/v1.0.0/webappnetworktester-windows.zip" -OutFile webapp-network-tester.zip -ProgressAction SilentlyContinue
 Expand-Archive webapp-network-tester.zip -DestinationPath \inetpub\wwwroot
+
+$config = @"
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+  <location path="." inheritInChildApplications="false">
+    <system.webServer>
+      <handlers>
+        <add name="aspNetCore" path="*" verb="*" modules="AspNetCoreModuleV2" resourceType="Unspecified" />
+      </handlers>
+      <aspNetCore processPath=".\webappnetworktester.exe"
+                  stdoutLogEnabled="false"
+                  stdoutLogFile=".\logs\stdout"
+                  hostingModel="inprocess" />
+    </system.webServer>
+  </location>
+</configuration>
+"@
+
+Set-Content \inetpub\wwwroot\web.config -Value $config
 
 # Force the IIS to restart
 net stop was /y
